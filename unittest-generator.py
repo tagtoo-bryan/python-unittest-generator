81a3ec4d48188948c70abc10a01b2169ba823747
tagtoo-bryan <bryan@tagtoo.org>
2015-06-11 13:07:00 +0800

    change the schema format and update the generator support exception

--- unittest-generator.py
+++ unittest-generator.py
@@ -15,10 +15,58 @@ for i in range(len(testlist)):
     testlist[i] = testlist[i].replace(".yml", "")
 
 
+def catch_output(config):
+    try:
+        if config["path"] == "":
+            _module = __import__(config["name"])
+        else:
+            _module = __import__(config["path"], fromlist = [config["name"]])
+    except ImportError:
+        # Display error message
+        return
+
+    for key, value in config["tests"].iteritems():
+        try:
+            _method = getattr(_module, key)
+        except:
+            pass
+
+        for case in value["cases"]:
+            try:
+                args = case.get("args", [])
+                kwargs = case.get("kwargs", {})
+                result = _method(*args, **kwargs)
+                case["result"] = result
+            except Exception as e:
+                case["result"] = e.__class__.__name__
+
+    return config
+
+
+def input_padding(config):
+    # pad "kwargs" key and add '"' for string input
+    
+    for key, value in config["tests"].iteritems():
+        for case in value["cases"]:
+            case["args"] = [] if "args" not in case else case["args"]
+            case["kwargs"] = {} if "kwargs" not in case else case["kwargs"]
+            for count, arg in enumerate(case["args"]):
+                if isinstance(arg, basestring):
+                    import pdb;pdb.set_trace()
+                    case["args"][count] = '"' + arg + '"'
+
+            for k, arg in case["kwargs"].iteritems():
+                if type(arg) == "str":
+                    case["kwargs"][k] = '"' + arg + '"'
+
+    return config
+
 def gen_model():
     for testcase in testlist:
         with open('./schemas/%s.yml' % testcase) as ifile:
             config = yaml.load(ifile)
+            config = catch_output(config)
+            config = input_padding(config)
             content = template.render(**config)
             with open('./test_files/test_%s.py' % config['name'], 'w') as ofile:
                 ofile.write(content)
