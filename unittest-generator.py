93bb09af376873102a28d466435fd7a2f6fb1452
tagtoo-bryan <bryan@tagtoo.org>
2015-06-27 22:00:43 +0800

    temp commit

--- unittest-generator.py
+++ unittest-generator.py
@@ -11,15 +11,31 @@ env = jinja2.Environment(
     loader=jinja2.FileSystemLoader('.')
 )
 template = env.get_template('unittest.pyt')
-testlist = os.listdir("./schemas")
-testlist = [name for name in testlist if ".yml" in name and ".swp" not in name]
+filelist = os.listdir("./schemas")
+filelist = [name for name in filelist if ".yml" in name and ".swp" not in name]
 
-for i in range(len(testlist)):
-    testlist[i] = testlist[i].replace(".yml", "")
+for i in range(len(filelist)):
+    filelist[i] = filelist[i].replace(".yml", "")
 
 
 
-def catch_output(config):
+
+def patch_func(func, *config):
+    def run_func(*args, **kwargs):
+        # config["inputs"]
+        # config["outputs"]
+        func(*args, **kwargs)
+
+    return run_func
+
+
+
+
+def set_mock_return_value(config, mocks)
+
+
+
+def test_mocks(config):
     try:
         if config["package"] == "":
             _module = __import__(config["module"])
@@ -29,96 +45,97 @@ def catch_output(config):
         # Todo: Display error message
         return
 
-    mock_modules = []
-    for module_str in config["mock"]["modules"]:
-        mock_modules.append(mock.patch(config["module"] + '.' + module_str))
-
+    mock_modules = [ config["module"] + '.' + key for key, value in config["mock"].iteritems() ]
     for key, value in config["methods"].iteritems():
         # config = parse_call(value["call"])
+        
+
+
 
         _attr = _module
+        with contextlib.nested(*mock_modules) as mocks:
+            for key, value in config["mock"].iteritems():
+                
+                
 
-        with contextlib.nested(mock_modules) as mocks:
             for call in value["calls"]:
                 try:
                     _attr = getattr(_attr, call["name"])
-                    if value["type"] == "last_method":
+                    if call["type"] == "last_method":
                         _method = _attr
-                    elif value["type"] == "method":
+                    elif call["type"] == "method":
                         call["args"] = [] if "args" not in call else call["args"]
                         call["kwargs"] = [] if "kwargs" not in call else call["kwargs"]
                         _attr = _attr(*call["args"], **call["kwargs"])
-                    elif value["type"] == "variable":
+                    elif call["type"] == "variable":
                         pass
                     else:
                         pass
-                except:
+                except Exception as e:
                     # Todo: Display error message
                     pass
 
             for case in value["cases"]:
                 try:
+                    case["args"] = [] if "args" not in case else case["args"]
+                    case["kwargs"] = [] if "kwargs" not in case else case["kwargs"]
                     result = _method(*case["args"], **case["kwargs"])
-                    if isinstance(result, mock.MagicMock):
-                        # I want to find which  mock object should be set return_value to make the result has a return value, not a MagicMock object
-                        name = re.search("name=\'[^']*\'", repr(result)).group().split('\'')[1]
-
-                        result = config["module"] + '.' + name
-                        case["type"] = "mock"
-                    else:
-                        case["type"] = "success"
                 except Exception as e:
-                    result = e.__class__.__name__
-                    case["type"] = "error"
-
-                case["result"] = repr(result)
+                    # print e.__class__.__name__
+                    pass
 
                 for index, m in enumerate(mocks):
-                    case["mocks"] = []
-                    for call in m.mock_calls:
-                        param_str = repr(call).split("call")[1]
-                        case["mocks"].append( {"module": mock_modules[index], "param_str": param_str} )
+                    for mcall in m.mock_calls:
+                        mcall_str = repr(mcall).replace("call", config["module"] + '.' + config["mock"]["modules"][index])
+                        mock_attrs.append(mcall_str)
+
+            config["methods"][key]["mocks"] = mock_attrs
 
 
     return config
 
 
-def input_padding(config):
-    # pad "kwargs" key and add '"' for string input
-    
+
+
+
+def transform_mock(config):
+    # remove dummy mocks & chain the mocks
     for key, value in config["methods"].iteritems():
-        for case in value:
-            case["args"] = [] if "args" not in case else case["args"]
-            case["kwargs"] = {} if "kwargs" not in case else case["kwargs"]
-            for count, arg in enumerate(case["args"]):
-                if isinstance(arg, basestring):
-                    case["args"][count] = repr(arg)
+        new_mocks = []
+        for m in value["mocks"]:
+            ms = m.replace(re.search("\([^)]+\)", m).group(), "") if re.search("\([^)]+\)", m) else m
+            pre = False
+            for new_mock in new_mocks:
+                if ms.startswith(new_mock):
+                    #pre = True
+                    break
 
-            for k, arg in case["kwargs"].iteritems():
-                if type(arg) == "str":
-                    case["kwargs"][k] = repr(arg)
+            if not pre:
+                new_mocks.append(ms)
+
+        config["methods"][key]["mocks"] = new_mocks
+
+    import pdb;pdb.set_trace()
 
     return config
-
-
 
 
 def gen_model():
-    for testcase in testlist:
+    for testcase in filelist:
         with open('./schemas/%s.yml' % testcase) as ifile:
             config = yaml.load(ifile)
-
-            import pdb;pdb.set_trace()
-
-
+            config = add_mocks_key_into_config(config)
             config = catch_output(config)
+            config = transform_mock(config)
+
+
             config = input_padding(config)
             content = template.render(**config)
             with open('./test_files/test_%s.py' % testcase, 'w') as ofile:
                 ofile.write(content)
 
 def run_test():
-    for testcase in testlist:
+    for testcase in filelist:
         try:
             os.system("python ./test_files/test_%s.py" % testcase)
         except:
